73b9fefaadb55888e7d9914821a06579fc12a93d
Mohammed elhadi Arneb <fm_arneb@si.dz>
2020-08-29 20:24:36 +0100

    First commit

---
+++ database/migrations/2020_08_23_200228_create_events_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->text('description');
+            $table->integer('edition');
+            $table->string('logo');
+            $table->timestamp('date_debut')->default(now());
+            $table->timestamp('date_fin')->default(now());
+            $table->integer('nombre_min_participant')->nullable();
+            $table->integer('number_max_participant')->nullable();
+            $table->enum('etat',['programe','encours','reporte','annule','termine']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <title>SaaS 1 — TheSaaS Sample Demo Landing Page</title>
+
+    <!-- Styles -->
+    <link href="{{('assets/css/page.min.css')}}" rel="stylesheet">
+    <link href="{{('assets/css/style.css')}}" rel="stylesheet">
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="{{('assets/img/apple-touch-icon.png')}}">
+    <link rel="icon" href="{{('assets/img/favicon.png')}}">
+  </head>
+
+  <body>
+
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light navbar-stick-dark" data-navbar="smart">
+      <div class="container">
+
+        <div class="navbar-left mr-4">
+          <button class="navbar-toggler" type="button">&#9776;</button>
+          <a class="navbar-brand" href="#">
+            <img class="logo-dark" src="../assets/img/logo-dark.png" alt="logo">
+            <img class="logo-light" src="../assets/img/logo-light.png" alt="logo">
+          </a>
+        </div>
+
+        <section class="navbar-mobile">
+          <nav class="nav nav-navbar mr-auto">
+            <a class="nav-link active" href="#home">Home</a>
+            <a class="nav-link" href="#section-features">Features</a>
+            <a class="nav-link" href="#section-pricing">Pricing</a>
+            <a class="nav-link" href="#">Resources</a>
+            <a class="nav-link" href="#">Contact</a>
+          </nav>
+
+          <div class="d-none d-stick-block">
+            <a class="btn btn-sm btn-light ml-lg-5 mr-2" href="#">Login</a>
+            <a class="btn btn-sm btn-success" href="#">Sign up</a>
+          </div>
+        </section>
+
+      </div>
+    </nav><!-- /.navbar -->
+
+
+    <!-- Header -->
+    <header id="home" class="header text-white h-fullscreen text-center text-lg-left" style="background-color: #24292e">
+      <canvas class="constellation" data-color="rgba(255,255,255,0.3)"></canvas>
+      <div class="container">
+        <div class="row align-items-center h-100">
+
+          <div class="col-lg-6">
+            <h1>How developers work</h1>
+            <p class="lead mt-5 mb-8">Support your workflow with lightweight tools and features. Then work how you work best—we'll follow your lead.</p>
+            <p class="gap-xy">
+              <a class="btn btn-round btn-outline-light mw-150" href="#">Learn more</a>
+              <a class="btn btn-round btn-light mw-150" href="#">Sign up</a>
+            </p>
+          </div>
+
+          <div class="col-lg-5 ml-auto">
+            <img class="mt-5" src="../assets/img/preview/laptop-1.png" alt="img">
+          </div>
+
+        </div>
+      </div>
+    </header><!-- /.header -->
+
+
+    <!-- Main Content -->
+    <main class="main-content">
+
+     
+
+
+      <!--
+      |‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒
+      | Features
+      |‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒
+      !-->
+      <section id="section-features" class="section bg-gray">
+        <div class="container">
+         
+
+          <div class="row gap-y align-items-center">
+            <div class="col-md-6 ml-auto">
+              <h4>Getting Started page</h4>
+              <p>As part of efforts to improve user onboarding, I shipped an experiment to a subset of new users showing them a Getting Started page immediately after signup to help them get to what we think are some of the most important actions for new users to take. We compared the subset of users against a control group to see whether such an experience improved engagement over a period of time.</p>
+            </div>
+
+            <div class="col-md-5 order-md-first">
+              <img class="rounded shadow-2" src="../assets/img/preview/dribbble-6.png" alt="...">
+            </div>
+          </div>
+
+
+          <hr class="my-8">
+
+
+          <div class="row gap-y align-items-center">
+            <div class="col-md-6 mr-auto">
+              <h4>Outdated comments toggling</h4>
+              <p>Last week we shipped outdated comments...well, brought them back, actually. We used to show collapsed comments on outdated diffs as a single line, but removed that when we shipped code review recently. We brought that back last week with an improvement: you can now toggle open threads on outdated diffs in a review individually.</p>
+            </div>
+
+            <div class="col-md-5">
+              <img class="rounded shadow-2" src="../assets/img/preview/dribbble-7.png" alt="...">
+            </div>
+          </div>
+
+
+          <hr class="my-8">
+
+
+          <div class="row gap-y align-items-center">
+            <div class="col-md-6 ml-auto">
+              <h4>Code review illustrations</h4>
+              <p>Less an illustration, and more a literal proxy. These show up when the feature's introduced. Monotonectally leverage existing standards compliant ideas with distributed data. Efficiently simplify cross-unit systems whereas adaptive testing. Monotonectally leverage existing standards compliant ideas with distributed data. Efficiently simplify cross-unit systems whereas adaptive testing.</p>
+            </div>
+
+            <div class="col-md-5 order-md-first">
+              <img class="rounded shadow-2" src="../assets/img/preview/dribbble-8.png" alt="...">
+            </div>
+          </div>
+
+
+        </div>
+      </section>
+
+
+      <!--
+      |‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒
+      | Pricing
+      |‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒
+      !-->
+      
+      <!--
+      |‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒
+      | Signup
+      |‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒
+      !-->
+      
+
+    </main>
+
+
+    <!-- Footer -->
+    <footer class="footer">
+      <div class="container">
+        <div class="row gap-y align-items-center">
+
+          <div class="col-md-3 text-center text-md-left">
+            <a href="#"><img src="../assets/img/logo-dark.png" alt="logo"></a>
+          </div>
+
+          <div class="col-md-6">
+            <div class="nav nav-center">
+              <a class="nav-link" href="#">About</a>
+              <a class="nav-link" href="#">Blog</a>
+              <a class="nav-link" href="#">Policy</a>
+              <a class="nav-link" href="#">Contact</a>
+            </div>
+          </div>
+
+          <div class="col-md-3 text-center text-md-right">
+            <small>© 2019. All rights reserved.</small>
+          </div>
+
+        </div>
+      </div>
+    </footer><!-- /.footer -->
+
+
+    <!-- Scripts -->
+    <script src="{{asset('assets/js/page.min.js')}}"></script>
+    <script src="{{asset('assets/js/script.js')}}"></script>
+
+  </body>
+</html>
